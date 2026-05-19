8387a387a6959de50006d439e718769c0d145d6a
Yurii Ovcharenko <ovyu@ukr.net>
2026-05-19 06:49:15 +0300

    9th commit

--- quantum_espresso_install.sh
+++ quantum_espresso_install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 cd  ~/tools/qe
+rm -rf *
 
 wget https://www.quantum-espresso.org/rdm-download/8/v7-5/c8778718560b4730b9acf1648d65e15b/qe-7.5-ReleasePack.tar.gz
 tar -xzvf qe-7.5-ReleasePack.tar.gz
 rm qe-7.5-ReleasePack.tar.gz
+cd ~/tools/qe/qe-7.5
 
 apt-get update
 apt-get install -y gfortran gcc g++ make libfftw3-dev libfftw3-mpi-dev libopenmpi-dev openmpi-bin
