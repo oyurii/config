b8ef23a61000a3888f054b15759e2926bb2ee756
Yurii Ovcharenko <ovyu@ukr.net>
2026-05-26 07:48:51 +0300

    12th commit

---
+++ lammps_connect.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+ rm -rf /root/tools/lammps                                                                                                                            
+5 ln -s /root/google_disk/MyDrive/MD_sim/lammps /root/tools/lammps                                                                                     
+6 chmod 744 /root/tools/lammps/src/lmp_mpi
+
